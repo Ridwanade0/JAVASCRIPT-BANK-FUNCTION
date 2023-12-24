acb7b13ce0ad540142d259f00a044dd28852ea46
Ridwan Oyeniyi <145073256+Ridwanade0@users.noreply.github.com>
2023-12-24 16:01:46 +0100

    Update to script.js
    achieved deposit function, for the function remaining history append

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("i think it is all working");
+const balance = document.querySelector("#balance");
+const history = document.getElementById("transaction-history");
+
+const confirmDepositBTN = document.getElementById("deposit-input-operation");
+
+let currentBalance = 0;
+
+confirmDepositBTN.addEventListener('click', function() {
+    let depositInput = document.getElementById("deposit-input-value");
+
+ 
+    let depositNumberValue = parseFloat(depositInput.value);
+
+    if (isNaN(depositNumberValue) || depositNumberValue == "" || depositNumberValue <= 0 ) {
+        alert("enter valid deposit amount")
+        return;
+    }
+
+    let newBalance = currentBalance + depositNumberValue;
+
+    console.log(newBalance);
+
+    currentBalance = newBalance;
+
+    balance.textContent = newBalance.toFixed(2);
+
+    depositInput.value = "";
+
+
+});
